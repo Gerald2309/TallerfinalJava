eee3bd5d07fdc9e1cd05511a615c71c555a190a5
Gerald2309 <68652749+Gerald2309@users.noreply.github.com>
2022-02-12 12:20:26 -0500

    Ejercicios 5 y 6 añadidos

--- src/main/java/com/sofka/tallerJava/Ejercicio6.java
+++ src/main/java/com/sofka/tallerJava/Ejercicio6.java
@@ -1,16 +1,11 @@
 package com.sofka.tallerJava;
 
-public class Ejercicio5 {
-    // TODO: 12/02/2022 Muestra los números impares y pares del 1 al 100 (ambos incluidos).
-    //  Usa un bucle while
+public class Ejercicio6 {
+    // TODO: 12/02/2022 Realiza el ejercicio anterior usando un ciclo for.
     public static void main(String[] args) {
-
-        int i = 1;
-        while (i <= 100)
-        {
+        for (int i = 0; i <= 100; i++){
             if (i % 2 == 0) System.out.println("El número "+ i +" es par");
             else System.out.println("El número "+ i +" es impar");
-            i ++;
         }
     }
 }
